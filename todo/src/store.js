4f12b6b61e0ac60efc757178904e9d058080c812
Herry-Jacker <herryjacker4@gmail.com>
2024-09-23 16:51:15 +0630

    fixed

--- todo/src/store.js
+++ todo/src/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
 
 export const store = configureStore({
-    reducer: {
-        todo: todoReducer,
-    }
-})
+	reducer: {
+		todo: todoReducer,
+	},
+});
