22ed8cf1bd55864d814e151b3d41cb5fe0d2e387
Soe Paing <herryjacker4@gmail.com>
2024-09-29 10:32:13 +0630

    todo ui and redux feature

--- todo/src/main.jsx
+++ todo/src/main.jsx
@@ -1,14 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-import { Provider } from 'react-redux';
-import { store } from './store.js';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
-)
+import { Provider } from "react-redux";
+import { store } from "./store.js";
+
+import { getTodo } from "./todoApi.js";
+
+store.dispatch(getTodo());
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+);
