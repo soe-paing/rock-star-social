31cf37283320a6c73d687fc2f8a7bab9fb91165e
Soe Paing <herryjacker4@gmail.com>
2024-09-29 20:01:49 +0630

    user login auth feature

--- social/src/pages/Login.jsx
+++ social/src/pages/Login.jsx
@@ -1,14 +1,63 @@
 import { Box, Typography, OutlinedInput, Button, Alert, } from "@mui/material";
+import { useRef, useState } from "react";
+import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
+
+async function postLogin(data) {
+    const res = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+}
 
 export default function Login() {
+    const navigate = useNavigate();
+
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+
+    const [ error, setError ] = useState();
+
+    const login = useMutation( postLogin, {
+        onSuccess: ({ token }) => {
+            localStorage.setItem("token", token);
+            navigate("/");
+        }
+    })
+
+    { error && (
+        <Alert
+            severity="warning"
+            sx={{ mb: 4 }}>
+            {error}
+        </Alert>
+    )}
     return <Box>
         <Typography variant="h3" sx={{ mb: 4 }}>Login</Typography>
 
         <Alert severity="warning" sx={{ mb: 4 }}>Username and Password required</Alert>
 
-        <form>
-            <OutlinedInput sx={{ mb: 2 }} fullWidth placeholder="Username" />
-            <OutlinedInput sx={{ mb: 2 }} fullWidth placeholder="Password" type="password" />
+        <form
+            onSubmit={ e => {
+                e.preventDefault();
+
+                const username = usernameRef.current.value;
+                const password = passwordRef.current.value;
+
+                if( !username || !password ) {
+                    return setError("require username and password");
+                }
+
+                login.mutate({username, password});
+
+                e.currentTarget.reset();
+            }}
+        >
+            <OutlinedInput inputRef={usernameRef} sx={{ mb: 2 }} fullWidth placeholder="Username" />
+            <OutlinedInput inputRef={passwordRef} sx={{ mb: 2 }} fullWidth placeholder="Password" type="password" />
 
             <Button variant="contained" type="submit" fullWidth>Login</Button>
         </form>
--- social/src/pages/Register.jsx
+++ social/src/pages/Register.jsx
@@ -1,6 +1,34 @@
 import { Box, Typography, OutlinedInput, Button, Alert } from "@mui/material";
+import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "react-query";
+
+async function postRegister(data) {
+	const res = await fetch("http://localhost:8080/register", {
+		method: "POST",
+		body: JSON.stringify(data),
+		headers: {
+			"Content-Type": "application/json",
+		}
+	})
+
+	return res.json();
+}
 
 export default function Register() {
+	const nameRef = useRef();
+	const usernameRef = useRef();
+	const profileRef = useRef();
+	const passwordRef = useRef();
+
+	const navigate = useNavigate();
+
+	const [ error, setError ] = useState();
+
+	const register = useMutation(postRegister, {
+		onSuccess: () => navigate("/login"),
+	})
+
 	return (
 		<Box>
 			<Typography
@@ -9,30 +37,53 @@ export default function Register() {
 				Register
 			</Typography>
 
-			<Alert
-				severity="warning"
-				sx={{ mb: 4 }}>
-				Username and Password required
-			</Alert>
+			{error && (
+				<Alert
+					severity="warning"
+					sx={{ mb: 4 }}>
+					{error}
+				</Alert>
+			)}
 
-			<form>
+			<form
+				onSubmit={ e => {
+					e.preventDefault();
+
+					const name = nameRef.current.value;
+					const username = usernameRef.current.value;
+					const profile = profileRef.current.value;
+					const password = passwordRef.current.value;
+
+					if(!name || !username || !password) {
+						return setError("require name, username and password");
+					}
+
+					register.mutate({ name, username, profile, password });
+
+					e.currentTarget.reset();
+				}}
+			>
 				<OutlinedInput
+					inputRef={nameRef}
 					sx={{ mb: 2 }}
 					fullWidth
 					placeholder="Name"
 				/>
 				<OutlinedInput
+					inputRef={usernameRef}
 					sx={{ mb: 2 }}
 					fullWidth
 					placeholder="Username"
 				/>
 				<OutlinedInput
+					inputRef={profileRef}
                     multiline
 					sx={{ mb: 2 }}
 					fullWidth
 					placeholder="Profile"
 				/>
 				<OutlinedInput
+					inputRef={passwordRef}
 					sx={{ mb: 2 }}
 					fullWidth
 					placeholder="Password"
